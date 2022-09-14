ab04b6cffb73c249abf09c0a6639edff6504e3d2
Joy <mmehedihasanjoyv@gmail.com>
2022-09-14 21:56:50 +0600

    solve the count down problem

--- script.js
+++ script.js
@@ -102,7 +102,6 @@ const closeModal = () => {
 const start = () => {
   // If already started, do not start again
   if (startTime) return;
-
   let count = 3;
   countdownOverlay.style.display = "flex";
 
@@ -113,7 +112,7 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
