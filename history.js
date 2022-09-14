9e2f7ccff654d6b482902e0ec51560de60a640df
Joy <mmehedihasanjoyv@gmail.com>
2022-09-14 23:35:52 +0600

    added decimal second in history

--- history.js
+++ history.js
@@ -1,6 +1,7 @@
 const histories = document.getElementById("histories");
 
 function addHistory(questionText, timeTaken, errorCount) {
+  console.log(decimalTime,'decimal');
   const newRow = document.createElement("div");
   newRow.classList.add("card");
 
@@ -26,12 +27,13 @@ function displayHistory() {
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
 
   previousTests.forEach((test) => {
+    console.log(test.timeTaken,'what is it');
     const newRow = document.createElement("div");
     newRow.classList.add("card");
 
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${test.timeTaken.toFixed()}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
